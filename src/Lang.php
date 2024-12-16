55f01ebd90e60d8efaf613f0be9b4073c5f04c2d
SerhiiCho <serhiicho@protonmail.com>
2024-12-16 13:15:41 +0200

    Change comments in Lang.php

--- src/Lang.php
+++ src/Lang.php
@@ -4,27 +4,25 @@ declare(strict_types=1);
 
 namespace Serhii\ShortNumber;
 
+/**
+ * @see https://github.com/short-number/short-number
+ */
 class Lang
 {
     private static string $lang = 'en';
 
     /**
-     * @var list<string>|null
+     * @var array<string,string>|null
      */
     private static array|null $overwrites;
 
     /**
      * Set the language by passing language short name
      * like 'en' or 'ru' as the argument.
-     * If given language is not supported by this package,
-     * its language will be set to English by default.
-     * If you don't call this method, the default
-     * language will be also English.
+     * Default is English.
      *
      * @param string $lang ISO 639-1 of the language
-     * @param list<string>|null $overwrites
-     *
-     * @see https://github.com/short-number/short-number
+     * @param array<string,string>|null $overwrites
      */
     public static function set(string $lang, array|null $overwrites = null): void
     {
