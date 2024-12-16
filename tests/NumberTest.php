d190aae4b9c865781b5e94ef56eee439690a8530
SerhiiCho <serhiicho@protonmail.com>
2024-12-16 09:39:36 +0200

    Simplified test

--- tests/NumberTest.php
+++ tests/NumberTest.php
@@ -5,31 +5,29 @@ declare(strict_types=1);
 namespace Serhii\Tests;
 
 use PHPUnit\Framework\Attributes\DataProvider;
-use Serhii\ShortNumber\Lang;
 use Serhii\ShortNumber\Number;
 
 final class NumberTest extends TestCase
 {
     #[DataProvider('providerForNumbers')]
-    public function testNumber(string $expect, string $lang, int $num): void
+    public function testNumber(string $expect, int $num): void
     {
-        Lang::set($lang);
         $this->assertSame($expect, Number::conv($num));
     }
 
     public static function providerForNumbers(): array
     {
         $testCases = [
-            ['1k', 'en', 1234],
-            ['4k', 'en', 4367],
-            ['5m', 'en', 5_935_235],
-            ['81m', 'en', 81_235_678],
-            ['3b', 'en', 3_456_789_012],
-            ['25b', 'en', 25_256_365_947],
-            ['91t', 'en', 91_345_678_912_345],
-            ['4q', 'en', 4_091_345_678_912_345],
-            ['13t', 'en', 13_000_000_000_000],
-            ['21t', 'en', 21_256_365_947_295],
+            ['1k', 1234],
+            ['4k', 4367],
+            ['5m', 5_935_235],
+            ['81m', 81_235_678],
+            ['3b', 3_456_789_012],
+            ['25b', 25_256_365_947],
+            ['91t', 91_345_678_912_345],
+            ['4q', 4_091_345_678_912_345],
+            ['13t', 13_000_000_000_000],
+            ['21t', 21_256_365_947_295],
         ];
 
         return self::addNegativeNumbers($testCases);
@@ -42,8 +40,8 @@ final class NumberTest extends TestCase
     private static function addNegativeNumbers(array $testCases): array
     {
         foreach ($testCases as $testCase) {
-            [$expect, $lang, $num] = $testCase;
-            $testCases[] = ["-{$expect}", $lang, -$num];
+            [$expect, $num] = $testCase;
+            $testCases[] = ["-{$expect}", -$num];
         }
 
         return $testCases;
